58fc7a5d4187faa1155061858591e61f4de0c281
Joshua Reitz <jreitz9@my.wgu.edu>
2025-07-06 16:35:49 -0400

    fetch, context

--- Contexts/GoalsContext.jsx
+++ Contexts/GoalsContext.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
+import { addDoc, collection, getDocs, onSnapshot } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 
 export const GoalsContext = createContext();
@@ -7,12 +7,31 @@ export const GoalsContext = createContext();
 export function GoalsProvider({ children }) {
   const [goals, setGoals] = useState([]);
 
-  async function fetchGoals() {}
+  async function fetchGoals() {
+    const snapshot = await getDocs(collection(db, "goals"));
+
+    const documents = snapshot.docs.map((doc) => {
+      return { ...doc.data(), id: doc.id };
+    });
+
+    setGoals(documents);
+  }
   async function createGoal(goalData) {
     await addDoc(collection(db, "goals"), goalData);
   }
   async function updateGoal() {}
   async function deleteGoal() {}
+
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "goals"), (snapshot) => {
+      const documents = snapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+      setGoals(documents);
+    });
+
+    return () => unsub();
+  }, []);
 
   return (
     <GoalsContext.Provider
--- app/goals/index.jsx
+++ app/goals/index.jsx
@@ -1,16 +1,54 @@
-import { View, Text, StyleSheet } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  Text,
+  StyleSheet,
+  FlatList,
+  Pressable,
+  View,
+  Modal,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useGoals } from "../../hooks/useGoals";
+import { useState } from "react";
 
 const Goals = () => {
+  const { goals } = useGoals();
+  const [selected, setSelected] = useState(null);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Your Goals</Text>
-
+      <FlatList
+        data={goals}
+        keyExtractor={(item, index) =>
+          item.id ? item.id.toString() : index.toString()
+        }
+        renderItem={({ item }) => (
+          <Pressable onPress={() => setSelected(item)}>
+            <View style={styles.goal}>
+              <Text style={{ margin: 16 }}>{item.goal}</Text>
+              <View style={[styles.progress, { width: `${item.progress}%` }]} />
+            </View>
+          </Pressable>
+        )}
+      />
+      {selected && (
+        <Modal animationType="slide" visible={selected !== null}>
+          <View style={styles.modal}>
+            <Text style={styles.title}>{selected.goal}</Text>
+            <Text>Adjust progress:</Text>
+            <View style={styles.buttonWrapper}>
+              <Pressable style={styles.btn} onPress={() => setSelected(null)}>
+                <Text>Close</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      )}
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Goals
+export default Goals;
 
 const styles = StyleSheet.create({
   container: {
@@ -19,7 +57,20 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 30,
   },
-})
+  goal: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    marginVertical: 10,
+    overflow: "hidden",
+    marginHorizontal: 30,
+  },
+  progress: {
+    height: 10,
+    backgroundColor: "#4caf50",
+    minWidth: 10,
+    borderRadius: 4,
+  },
+});
